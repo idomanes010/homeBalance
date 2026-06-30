7e6e7a872315169bfd27f3c64943baa49af1c6e9
idomanes010 <idomanes010@gmail.com>
2026-06-30 13:53:16 +0300

    Remove unused Layout import

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,7 +1,6 @@
-// import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Layout } from './Components/LayoutArea/Layout/Layout'
 import './index.css'
 import { AuthProvider } from './Context/AuthContext'
 import { Routing } from './Components/LayoutArea/Routing/Routing'
